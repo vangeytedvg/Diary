b08a5746f711b5117eb602502178aa20ef10d3b8
vangeytedvg <vangeytedvg@gmail.com>
2021-05-13 11:29:30 +0200

    Auto

--- gitter.sh
+++ gitter.sh
@@ -1,8 +1,11 @@
-echo "Start Git script"
+echo "Starting Git "
 git add *
 echo "Comments?  "
 read comment
+echo "EXECUTING :" $comment 
 
 git commit -m $comment
 
 git push -u origin main
+
+echo "DONE...."
