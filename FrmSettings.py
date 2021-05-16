f99d851523ae403d0a15c8c334d7f2f38971cd25
vangeytedvg <vangeytedvg@gmail.com>
2021-05-16 14:13:43 +0200

    Settings form colors settings

--- FrmSettings.py
+++ FrmSettings.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog, QColorDialog
 from PyQt5.QtCore import QDate
 from frmSettings import Ui_frmSettings
 from utilities.setting import Settings
@@ -17,16 +17,29 @@ class FrmSettings(QDialog, Ui_frmSettings):
         super(FrmSettings, self).__init__()
         self.setupUi(self)
         self.parent = parent
+        # settings methods and members
+        self.__color_weekday_background = ""
         self.load_settings()
         # Actions
         self.btnSave.clicked.connect(self.save_and_close)
         self.btnClose.clicked.connect(self.close)
         self.btnOpenFolder.clicked.connect(self.choose_folder)
+        self.btn_color_weekday_background.clicked.connect(self.set_color_workday_background)
+
+    def set_color_workday_background(self):
+        self.__color_weekday_background = QColorDialog(parent=self).getColor()
+        self.lbl_color_weekday_background.setStyleSheet("background-color: %s" % self.__color_weekday_background.name())
 
     def load_settings(self):
         params = Settings(self, "DenkaTech", "KDiary")
+        # file settings
         file_path = params.load_setting("paths", "file_location")
         self.txtPathToDiary.setText(file_path)
+        # color settings
+        self.__color_weekday_background = params.load_setting("colors", "weekday_background")
+        print("COLO", self.__color_weekday_background)
+        if self.__color_weekday_background:
+            self.lbl_color_weekday_background.setStyleSheet("background-color: %s" % self.__color_weekday_background)
 
     def save_and_close(self):
         """
@@ -34,6 +47,7 @@ class FrmSettings(QDialog, Ui_frmSettings):
         """
         params = Settings(self, "DenkaTech", "KDiary")
         params.save_setting("paths", "file_location", self.txtPathToDiary.text())
+        params.save_setting("colors", "weekday_background", self.__color_weekday_background.name())
         self.close()
 
     def choose_folder(self):
@@ -42,8 +56,8 @@ class FrmSettings(QDialog, Ui_frmSettings):
         """
         dir = QFileDialog.getExistingDirectory(self, "Open Directory",
                                                "/home",
-                                               QFileDialog.ShowDirsOnly |
-                                               QFileDialog.DontResolveSymlinks)
+                                               QFileDialog.ShowDirsOnly
+                                               | QFileDialog.DontResolveSymlinks)
         if dir:
             self.txtPathToDiary.setText(dir)
 
