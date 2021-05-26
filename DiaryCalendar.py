20d34aa049cec92ed89f1d49edb39bb3b4d6eb30
vangeytedvg <vangeytedvg@gmail.com>
2021-05-26 15:02:29 +0200

    Backup

--- DiaryCalendar.py
+++ DiaryCalendar.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt, QRectF, QDate
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtWidgets import QCalendarWidget
 from fileman import FileManager as fm
 import csv
@@ -26,7 +26,7 @@ class DiaryCalendar(QCalendarWidget):
                  color_select_background,
                  color_select_foreground):
         self.holidays = []
-        self.load_holidays()
+        self.load_holidays(QDate().currentDate().year())
 
         """
         Constructore override, need the path to the diary pages,
@@ -58,26 +58,44 @@ class DiaryCalendar(QCalendarWidget):
         """ Check if the painted date is a holiday """
         return [holiday for holiday in self.holidays if holiday['Date'] == thedate]
 
-    def load_holidays(self):
+    def load_holidays(self, year):
         """ Load holidays file and add easter day to the recovered data """
         with open('specialdays.csv', newline='') as holidays_file:
             reader = csv.DictReader(holidays_file)
             self.holidays = list(reader)
         # Add the date of easter to the holidays list
-        easter = self.calc_easter(2021)
+        easter = self.calc_easter(year)
+        day = str(easter.day()).zfill(2)
+        month = str(easter.month()).zfill(2)
+        easter = day + month
+        # Convert to a dictionnary here
+        element = {'Date': easter, 'Name': 'Easter'}
+        self.holidays.append(element)
 
     def paintCell(self, painter, rect, date):
+        self.load_holidays(date.year())
+        painter.fillRect(rect, self.myQColorWE)
+        holiday_font = painter.font()
+        holiday_font.setPixelSize(12)
+        painter.setFont(holiday_font)
+
         painter.setRenderHint(QPainter.Antialiasing, True)
         filename = self._file_path + "/" + fm.make_diary_filename(date.year(), date.month(), date.day())
 
         # check if the drawing date is a holiday
         str_date = str(date.day()).zfill(2) + str(date.month()).zfill(2)
+        hday = self.is_date_holiday(str_date)
 
         if self.is_date_holiday(str_date):
             print("Holiday ", self.is_date_holiday(str_date)[0]['Name'])
 
         if fm.page_exists(filename):
             # We have a diary entry for this date, let the user know
+            painter.fillRect(rect, self.myQColorWE)
+            holiday_font = painter.font()
+            holiday_font.setPixelSize(12)
+            painter.setFont(holiday_font)
+
             painter.save()
             if date.dayOfWeek() > 5:
                 # Weekend colors
@@ -93,7 +111,31 @@ class DiaryCalendar(QCalendarWidget):
                 painter.fillRect(rect, self.myQColor_sel_bg)
             # painter.drawText(QRectF(rect), Qt.TextSingleLine | Qt.AlignBottom, "text")
             painter.drawText(QRectF(rect), Qt.TextSingleLine | Qt.AlignCenter, str(date.day()))
+            if self.is_date_holiday(str_date):
+                holiday_font = painter.font()
+                holiday_font.setPixelSize(9)
+                painter.setFont(holiday_font)
+                painter.drawText(QRectF(rect), Qt.TextSingleLine | Qt.AlignBottom, hday[0]['Name'])
+                holiday_font.setPixelSize(12)
+                painter.setFont(holiday_font)
             painter.restore()
         else:
             # Just a normal date, no diary entry
-            QCalendarWidget.paintCell(self, painter, rect, date)
+            if self.is_date_holiday(str_date):
+                painter.save()
+                painter.fillRect(rect, self.myQColorWE)
+                holiday_font = painter.font()
+                holiday_font.setPixelSize(12)
+                painter.setFont(holiday_font)
+                QCalendarWidget.paintCell(self, painter, rect, date)
+                painter.drawText(QRectF(rect), Qt.TextSingleLine | Qt.AlignCenter, str(date.day()))
+                holiday_font.setPixelSize(9)
+                painter.setFont(holiday_font)
+                painter.drawText(QRectF(rect), Qt.TextSingleLine | Qt.AlignBottom, hday[0]['Name'])
+                holiday_font.setPixelSize(12)
+                painter.setFont(holiday_font)
+
+            else:
+                QCalendarWidget.paintCell(self, painter, rect, date)
+
+
