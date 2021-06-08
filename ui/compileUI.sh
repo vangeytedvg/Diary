b74a6bb3ee48815a57d7e7d962cd6052eb9c8cfb
vangeytedvg <vangeytedvg@gmail.com>
2021-06-08 16:28:50 +0200

    Correctly makes the fist letter of the filename

--- ui/compileUI.sh
+++ ui/compileUI.sh
@@ -1,9 +1,15 @@
 DIR=$(pwd)
 echo "$DIR"
+echo "Compiling GUI files"
 for FILE in "$DIR"/*.ui; do
+  # Show what is compiling
   echo "Compiling ${FILE##*/}"
-  test=$(basename "$FILE" .ui)
-  echo "frm_$test"
+  # Get the basename of the file (no extension). We know the extension
+  # is .ui, so get rid of it
+  source=$(basename "$FILE" .ui)
+  # Make the target filename, the syntax for bash 4 will uppercase the
+  # first letter of the filename
+  echo "frm${source^}".py
   #pyuic5 -o test.py "$f"
 done
 
